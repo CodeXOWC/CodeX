f773561ddd4e7da57b81c5a6a83af18b781a08e5
Justin Hayes <justin@justinbhayes.com>
2012-03-25 06:41:21 -0500

    footer style updates

--- wp-content/themes/retlehs-roots/footer.php
+++ wp-content/themes/retlehs-roots/footer.php
@@ -5,54 +5,7 @@
   <footer id="content-info" class="<?php echo WRAP_CLASSES; ?>" role="contentinfo">
     <?php roots_footer_inside(); ?>
     <?php dynamic_sidebar('roots-footer'); ?>
-    <div>
-    	<h2><a href="#" class="parent">About Us</a></h2>
-      <ul>
-      	<li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-    </div>
-    <div>
-    	<h2><a href="#" class="parent">Homelessness</a></h2>
-      <ul>
-      	<li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-    </div>
-    <div>
-    	<h2><a href="#" class="parent">Donate</a></h2>
-      <ul>
-      	<li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-    </div>
-    <div>
-    	<h2><a href="#" class="parent">In the News</a></h2>
-      <ul>
-      	<li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-    </div>
-    <div>
-    	<h2><a href="#" class="parent">Get Involved</a></h2>
-      <ul>
-      	<li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-    </div>
-    <div>
-    	<h2><a href="#" class="parent">Campaigns</a></h2>
-      <ul>
-      	<li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
-      </ul>
-    </div>
+    <?php wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Navbar_Nav_Walker(), 'menu_class' => 'bottom_nav')); ?>
   </footer>
   <?php roots_footer_after(); ?>
 

fc59ff844e68c8eee8f1674340d659216925aea1
Justin Hayes <justin@justinbhayes.com>
2012-04-01 16:22:29 -0500

    added tooltip script

--- wp-content/themes/retlehs-roots/header.php
+++ wp-content/themes/retlehs-roots/header.php
@@ -20,6 +20,7 @@
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/libs/hoverIntent.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/libs/bootstrap-tooltip.js"></script>
   <script src="<?php echo get_template_directory_uri(); ?>/js/libs/functions.js"></script>
 
   <?php roots_head(); ?>
